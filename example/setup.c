18d587078c220ef2498c7d8083caef13cb7bb64c
Remmery Julien <julien.remmery@student.vinci.be>
2025-09-26 03:37:45 +0200

    Fix setup function calls to include db_type parameter for consistency

--- example/setup.c
+++ example/setup.c
@@ -12,14 +12,22 @@ int main(void)
         // CRUD functions to include (CREATE, READ, UPDATE, DELETE, CRUD for all)
         .crud = CRUD,                       
         // Include debug functions (print_*, ...)
-        .options = DEBUG,                   
+        .options = DEBUG,
+        // Type of database you plan to use
+        .db_type = JSON_DATABASE,                   
     };
-    corm_setup_(context);    
+    corm_setup(context);    
     return 0;
 }
 
 int main2(void)
 {
-    corm_setup(.input_header = "example/user.h", .output_path = "example/", .crud = CRUD, .options = DEBUG);    
+    corm_setup_(
+        .input_header = "example/user.h", 
+        .output_path = "example/", 
+        .crud = CRUD, 
+        .options = DEBUG,
+        .db_type = JSON_DATABASE
+    );    
     return 0;
 }
